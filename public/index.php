7b931419d65c9c452810304ad7713fffce266df6
Apple <appleeyespackage@gmail.com>
2023-09-14 15:34:56 +0100

    Initial commit

--- public/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once __DIR__ . '/../vendor/autoload.php';
-require_once __DIR__ . '/../src/config/database.php';
-require_once __DIR__ . '/../src/routes/movies.php';
-
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
-$dotenv->load();
-
